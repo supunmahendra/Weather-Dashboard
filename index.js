6b80afe26878f35637a68445a8f645e303afacd6
Supun Mahendra <supunmahendra@gmail.com>
2024-03-10 20:16:09 +0100

    get the value from search and fetch

--- index.js
+++ index.js
@@ -5,21 +5,34 @@
 //Weather London API : https://api.openweathermap.org/data/2.5/weather?q=London&appid=deb41da100a4adf43b939c1e823043cb
 
 var ulr;
-var city = "London";
+var city;
 const apiKey = "deb41da100a4adf43b939c1e823043cb"
-ulr = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey
-
-console.log(ulr);
 
 
+// button action
+const btn = document.querySelector("#search-button")
+btn.addEventListener("click",() => {search()})
 
-fetch(ulr).then((response)=>{
-    console.log(response);
-    return response.json();
-}).then((data)=>{
-    const weather= data;
-    console.log(weather);
-    productsPrint(weather);
-}).catch((error)=>{
-    console.log(error);
-})
+
+// function start hear
+function search() {
+    city = document.getElementById("search-input").value;
+    console.log(city);
+    ulr = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
+    fetchData(ulr);
+}
+
+
+
+
+function fetchData(ulr){
+    fetch(ulr).then((response)=>{
+       return response.json();
+    }).then((data)=>{
+        const weather= data;
+        console.log(weather);
+        productsPrint(weather);
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
